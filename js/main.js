a7f3860358c790e64cde561f3b56627f5c5475c5
serverd82 <serverd@mail.ua>
2021-03-24 01:27:26 +0300

    create: modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper(".hotel-slider", {
+$(document).ready(function () {
+  const hotelSlider = new Swiper(".hotel-slider", {
   // Optional parameters
   loop: true,
 
@@ -33,6 +34,27 @@ const reviewsSlider = new Swiper(".reviews-slider", {
 
 var menuButton = document.querySelector(".menu-button");
 menuButton.addEventListener("click", function() {
-  console.log('Клик по кнопке меню');
   document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
+});
+
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal() {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.addClass("modal__overlay--visible");
+  modalDialog.addClass("modal__dialog--visible");
+}
+function closeModal (event) {
+  event.preventDefault();
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+  modalOverlay.removeClass("modal__overlay--visible");
+  modalDialog.removeClass("modal__dialog--visible");
+  
+}
+
 });
